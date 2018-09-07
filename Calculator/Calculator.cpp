633c4635fcef2a9d573fd5737b7f681223b4be0c
Egor Smirnov <s.e.a.98@yandex.ru>
2018-09-07 16:31:46 +0300

    initial commit

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,12 +1,158 @@
-﻿// Calculator.cpp : Defines the entry point for the application.
-//
+﻿#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
-#include "Calculator.h"
+namespace AST {
+	enum Types { Number, Sign };
 
-using namespace std;
+	struct Token {
+		union {
+			double fractional;
+			int64_t integer;
+			struct {
+				int32_t priority;
+				char val;
+			} sign;
+		};
+		Types type;
+	};
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+	const std::unordered_map<char, int> signToPriority = {
+		{'+', 1}, {'-', 1}, {'*', 2}, {'^', 3}, {'/', 2}, {'|', 2}, {'%', 2} };
+
+	const std::unordered_map<char, double> constants = { {'e', 2.7}, {'p', 3.4} };
+
+	const std::int32_t adder =
+		std::max_element(signToPriority.begin(), signToPriority.end(),
+			[](auto l, auto r) { return l.second < r.second; })
+		->second +
+		1;
+
+	const std::size_t end = std::numeric_limits<std::size_t>::max();
+
+	class Tokenizer {
+	private:
+		std::string str;
+
+	public:
+		Tokenizer(std::string &str) : str(str) {}
+
+		std::vector<Token> Tokenize() {
+			std::vector<Token> result;
+
+			std::size_t numberStart =
+				end; // presumably you'll run out of memory the way sooner
+			std::size_t numberEnd = end;
+
+			std::int32_t lvl = 0;
+			auto it = signToPriority.end();
+
+			for (std::size_t i = 0; i <= str.size();
+				++i) { // string.c_str() - null-terminated array of char
+				if (std::isdigit(str[i])) {
+					if (numberStart == end)
+						numberStart = numberEnd = i;
+					else
+						++numberEnd;
+				}
+				else if (numberEnd != end) {
+					Token t;
+
+					t.type = Types::Number;
+					t.integer = std::stoi(std::string(str, numberStart, numberEnd + 1));
+
+					result.push_back(t);
+					numberStart = numberEnd = end;
+				}
+
+				if (str[i] == '(')
+					++lvl;
+				else if (str[i] == ')') {
+					--lvl;
+					if (lvl < 0)
+						throw std::exception("Brackets do not match");
+				}
+
+				auto it = signToPriority.find(str[i]);
+
+				if (it != signToPriority.end()) {
+					Token t;
+
+					t.type = Types::Sign;
+					t.sign.priority = lvl * adder + signToPriority.find(str[i])->second;
+					t.sign.val = str[i];
+
+					result.push_back(t);
+				}
+			}
+
+			if (lvl != 0)
+				throw std::exception("Brackets do not match");
+
+			return result;
+		}
+	};
+
+	template <class T> T eval(char ch, T &lhs, T &rhs) {
+		static_assert(std::is_integral<T>::value, "Integral required.");
+		static_assert(std::is_floating_point<T>::value, "Floating point required.");
+
+		switch (ch) {
+		case '+':
+			return lhs + rhs;
+		case '-':
+			return lhs - rhs;
+		case '*':
+			return lhs * rhs;
+		case '/':
+			return lhs / rhs;
+		case '^':
+			return std::pow(lhs, rhs);
+		case '|':
+			return 0; // check later
+		case '%':
+			return 0; // check later
+
+		default:
+			throw std::exception("Wrong operator: " + ch);
+		}
+	};
+}; // namespace AST
+
+int main() {
+	std::string s = "1 * (23 / 123 ^ 1) + (123 + 23 + 1) + 3";
+	std::cout << s << std::endl << std::endl;
+
+	// std::cin >> s;
+
+	AST::Tokenizer T(s);
+
+	try {
+		auto res = T.Tokenize();
+
+		for (auto &t : res) {
+			if (t.type == AST::Types::Number)
+				std::cout << "Number: " << t.integer;
+			else {
+				std::cout << "Sign: " << t.sign.val
+					<< ", priority: " << t.sign.priority;
+			}
+
+			std::cout << std::endl;
+		}
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cin.get();
+
 	return 0;
 }
