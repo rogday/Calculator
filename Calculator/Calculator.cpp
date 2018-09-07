89ecdec7f573f6b017298f3c5a98d66eb5077a49
Egor Smirnov <s.e.a.98@yandex.ru>
2018-09-07 16:30:52 +0300

    Add project files.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,12 @@
+﻿// Calculator.cpp : Defines the entry point for the application.
+//
+
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
